3caf5f426cddde3a108b9a8ad58b820bce46117a
Diego Urrego <diegourrg@gmail.com>
2022-09-21 01:16:47 -0500

    Se agrega funcionalidad de guardado en el localStorage de los Todos.

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,14 +1,17 @@
 export class TodoList {
   constructor() {
-    this.todos = [];
+    // this.todos = [];
+    this.cargarLocalStorage();
   }
 
   nuevoTodo(todo) {
     this.todos.push(todo);
+    this.guardarLocalStorage();
   }
 
   eliminarTodo(id) {
     this.todos = this.todos.filter((todo) => todo.id != id);
+    this.guardarLocalStorage();
   }
 
   marcarCompletado(id) {
@@ -16,6 +19,7 @@ export class TodoList {
       console.log(id, todo.id);
       if (todo.id == id) {
         todo.completado = !todo.completado;
+        this.guardarLocalStorage();
         break;
       }
     }
@@ -23,9 +27,16 @@ export class TodoList {
 
   eliminarCompletados() {
     this.todos = this.todos.filter((todo) => !todo.completado);
+    this.guardarLocalStorage();
   }
 
-  guardarLocalStorage() {}
+  guardarLocalStorage() {
+    localStorage.setItem('todo', JSON.stringify(this.todos));
+  }
 
-  cargarLocalStorage() {}
+  cargarLocalStorage() {
+    this.todos = localStorage.getItem('todo')
+      ? JSON.parse(localStorage.getItem('todo'))
+      : [];
+  }
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,4 @@ import { crearTodoHtml } from './js/componentes';
 import './styles.css';
 
 export const todoList = new TodoList();
-const tarea = new Todo('Aprender Javascript!!');
-
-todoList.nuevoTodo(tarea);
-
-console.log(todoList);
-
-crearTodoHtml(tarea);
-
-// localStorage.setItem('mi-key', 'ABC123');
-
-// setTimeout(() => {
-//   localStorage.removeItem('mi-key');
-// }, 1500);
+todoList.todos.forEach(crearTodoHtml);
